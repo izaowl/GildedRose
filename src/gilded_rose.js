ccbe4b0a26603663787cddd4f6a5da8ab8942ce0
izaowl <izasow@yahoo.com>
2022-03-21 17:04:10 +0000

    Updated system with new requirement for  conjured items devaluation of quality

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -15,9 +15,11 @@ class Shop {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
-          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
+          if (this.items[i].name != 'Sulfuras, Hand of Ragnaros' || this.items[i].name != 'Conjured Mana Cake') {
             this.items[i].quality = this.items[i].quality - 1;
           }
+          if (this.items[i].name == 'Conjured Mana Cake')
+          this.items[i].quality = this.items[i].quality - 2;
         }
       } else {
         if (this.items[i].quality < 50) {
@@ -69,13 +71,13 @@ class Shop {
 
   const items = [
     new Item("+5 Dexterity Vest", 10, 20),
-    new Item("Aged Brie", 2, 0),
-    new Item("Elixir of the Mongoose", 5, 7),
-    new Item("Sulfuras, Hand of Ragnaros", 0, 80),
-    new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-    new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+    // new Item("Aged Brie", 2, 0),
+    // new Item("Elixir of the Mongoose", 5, 7),
+    // new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+    // new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+    // new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+    // new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+    // new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
   
     // This Conjured item does not work properly yet
     //new Item("Conjured Mana Cake", 3, 6),
